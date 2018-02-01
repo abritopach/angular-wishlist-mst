6c14bafe6fdfc207a3fd004e59e125dcf4825281
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-02-01 17:19:15 +0100

    Added actions & new test for actions

--- src/models/wishlist.model.spec.ts
+++ src/models/wishlist.model.spec.ts
@@ -11,6 +11,9 @@ it('should create a instance of a WishListItem model', () => {
 
     expect(item.price).toBe(28.73);
     expect(item.image).toBe('');
+
+    item.changeName('Narnia');
+    expect(item.name).toBe('Narnia');
 });
 
 it('should create a WishList', () => {
@@ -26,4 +29,17 @@ it('should create a WishList', () => {
 
     expect(list.items.length).toBe(1);
     expect(list.items[0].price).toBe(28.73);
+});
+
+it('should add new item to WishList', () => {
+    const list = WishList.create();
+    list.add(WishListItem.create({
+        name: 'Chesterton',
+        price: 10
+    }));
+
+    expect(list.items.length).toBe(1);
+    expect(list.items[0].name).toBe('Chesterton');
+    list.items[0].changeName('Book of G.K. Chesterton');
+    expect(list.items[0].name).toBe('Book of G.K. Chesterton');
 });
--- src/models/wishlist.model.ts
+++ src/models/wishlist.model.ts
@@ -12,9 +12,24 @@ export const WishListItem = types.model({
     name: types.string,
     price: types.number,
     image: types.optional(types.string, '')
-});
+}).actions(self => ({
+
+    changeName(newName) {
+        self.name = newName;
+    },
+    changePrice(newPrice) {
+        self.price = newPrice;
+    },
+    changeImage(newImage) {
+        self.image = newImage;
+    }
+}));
 
 export const WishList = types.model({
     items: types.optional(types.array(WishListItem), [])
-});
+}).actions(self => ({
+    add(item) {
+        self.items.push(item);
+    }
+}));
 
